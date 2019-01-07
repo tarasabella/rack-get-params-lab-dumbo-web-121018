2d14440ff20eff438976abbf96513ddc74786b5d
Tara Sabella <tarasabella@gmail.com>
2019-01-07 21:28:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -8,15 +9,32 @@ class Application
 
     if req.path.match(/items/)
       @@items.each do |item|
-        resp.write "#{item}\n"
+      resp.write "#{item}\n"
       end
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    else
+    end 
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Hey, your cart is empty"
+      else 
+      @@cart.each do |item| 
+      resp.write "#{item}\n"
+    end 
+  end 
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include? item_to_add 
+        @@cart << item_to_add
+        resp.write "added #{item_to_add}"
+      else
+        resp.write "We don't have that item!"
+      end
+    else 
       resp.write "Path Not Found"
     end
-
     resp.finish
   end
 
@@ -27,4 +45,4 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
-end
+end 
